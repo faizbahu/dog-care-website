215ff9f75ea22a6a1980fe7634bd88d84e1aa2ef
JawadR <jawadrasheed97@gmail.com>
2023-02-05 14:48:49 +0500

    Installed Laravel and Breeze

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                primary_gray: "#232323",
+                primary_purple: "#48297a",
+                primary_black: "#6b7280"
+            },
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
